f072d0f65237a803a2957d5085c60c874eaf6815
RafatMeraz <razhossain201@gmail.com>
2022-12-13 22:58:15 +0600

    live class 21 code has been uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,225 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeState extends State<Home> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Counter app'),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton.extended(
+            onPressed: () {
+              counter++;
+              setState(() {});
+            },
+            label: Row(
+              children: [
+                Icon(Icons.add),
+                Text('Add'),
+              ],
+            ),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton.extended(
+            onPressed: () {
+              if (counter > 0) {
+                counter--;
+                setState(() {});
+              }
+            },
+            label: Row(
+              children: [
+                Icon(Icons.remove),
+                Text('Remove'),
+              ],
+            ),
+          )
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              counter.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
             ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SecondScreen()));
+                },
+                child: Text('Go to second screen'))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  SecondScreen({Key? key}) : super(key: key);
+
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Column(
+        children: [
+          Column(),
+          Row(),
+          Row(),
+          StatefulBuilder(builder: (context, setState) {
+            return Row(
+              children: [
+                Text(counter.toString()),
+                Spacer(),
+                ElevatedButton(
+                    onPressed: () {
+                      counter++;
+                      setState(() {});
+                    },
+                    child: Text('change name'))
+              ],
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  TextEditingController inputTextFieldController = TextEditingController();
+  TextEditingController inputTwoTextFieldController = TextEditingController();
+
+  double result = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: inputTextFieldController,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: '0',
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: inputTwoTextFieldController,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: '0',
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text('Result = $result'),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      final double firstValue =
+                          double.tryParse(inputTextFieldController.text) ?? 0;
+                      final double secondValue =
+                          double.tryParse(inputTwoTextFieldController.text) ??
+                              0;
+                      result = firstValue + secondValue;
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.add)),
+                IconButton(
+                    onPressed: () {
+                      final double firstValue =
+                          double.tryParse(inputTextFieldController.text) ?? 0;
+                      final double secondValue =
+                          double.tryParse(inputTwoTextFieldController.text) ??
+                              0;
+                      result = firstValue - secondValue;
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.remove)),
+                IconButton(
+                    onPressed: () {
+                      final double firstValue =
+                          double.tryParse(inputTextFieldController.text) ?? 0;
+                      final double secondValue =
+                          double.tryParse(inputTwoTextFieldController.text) ??
+                              0;
+                      result = firstValue * secondValue;
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.star)),
+                IconButton(
+                    onPressed: () {
+                      final double firstValue =
+                          double.tryParse(inputTextFieldController.text) ?? 0;
+                      final double secondValue =
+                          double.tryParse(inputTwoTextFieldController.text) ??
+                              0;
+                      result = firstValue / secondValue;
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.safety_divider)),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
