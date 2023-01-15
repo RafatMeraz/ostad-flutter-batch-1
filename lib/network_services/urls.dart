b9c142948e0575e54b9ca9c07fadb1b546bd4a1b
RafatMeraz <razhossain201@gmail.com>
2023-01-15 22:05:34 +0600

    task status change api has been implemented

--- lib/network_services/urls.dart
+++ lib/network_services/urls.dart
@@ -1,4 +1,7 @@
 class Urls {
   static String baseUrl = 'https://task.teamrabbil.com/api/v1';
   static String newTask = '$baseUrl/listTaskByStatus/New';
+
+  static String changeStatus(String taskId, String status) =>
+      '$baseUrl/updateTaskStatus/$taskId/$status';
 }
--- lib/screens/new_task_screen.dart
+++ lib/screens/new_task_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:ostad_flutter_batch_one/models/new_task_model.dart';
 import 'package:ostad_flutter_batch_one/network_services/network_requester.dart';
 import 'package:ostad_flutter_batch_one/network_services/urls.dart';
+import 'package:ostad_flutter_batch_one/widgets/text_styles.dart';
 
+import '../widgets/reusable_elevated_button.dart';
 import '../widgets/task_widget.dart';
 
 class NewTaskScreen extends StatefulWidget {
@@ -27,9 +29,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     final response = await NetworkRequester().getRequest(Urls.newTask);
     if (response['status'] == 'success') {
       _newTaskModel = NewTaskModel.fromJson(response);
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
@@ -53,22 +53,104 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
         if (_newTaskModel == null)
           const Expanded(child: Center(child: CircularProgressIndicator()))
         else
-          Expanded(child: ListView.builder(
-            itemCount: _newTaskModel?.tasks?.length ?? 0,
-            itemBuilder: (context, index) {
-              final task = _newTaskModel!.tasks![index];
-              return TaskWidget(
-                title: task.title ?? 'Unknown',
-                description: task.description ?? 'UnKnown',
-                date: task.createdDate ?? 'Unknown',
-                onDeleteTap: () {},
-                onEditTap: () {},
-                type: task.status ?? 'New',
-              );
+          Expanded(
+              child: RefreshIndicator(
+            onRefresh: () async {
+              getNewTaskFromApi();
             },
+            child: ListView.builder(
+              itemCount: _newTaskModel?.tasks?.length ?? 0,
+              itemBuilder: (context, index) {
+                final task = _newTaskModel!.tasks![index];
+                return TaskWidget(
+                  title: task.title ?? 'Unknown',
+                  description: task.description ?? 'UnKnown',
+                  date: task.createdDate ?? 'Unknown',
+                  onDeleteTap: () {},
+                  onEditTap: () {
+                    showModalSheetForChangeStatus(task.sId ?? '');
+                  },
+                  type: task.status ?? 'New',
+                );
+              },
+            ),
           ))
       ],
     );
+  }
+
+  void showModalSheetForChangeStatus(String taskId) {
+    String taskStatus = 'In Progress';
+
+    bool taskStatusChangeInProgress = false;
+
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, changeState) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const Text('Change status of task',
+                      style: pageTitleTextStyle),
+                  const SizedBox(height: 24),
+                  RadioListTile(
+                      title: const Text('In Progress'),
+                      value: 'In Progress',
+                      onChanged: (value) {
+                        taskStatus = value!;
+                        changeState(() {});
+                      },
+                      groupValue: taskStatus),
+                  RadioListTile(
+                    groupValue: taskStatus,
+                    title: const Text('Completed'),
+                    value: 'Completed',
+                    onChanged: (value) {
+                      taskStatus = value!;
+                      changeState(() {});
+                    },
+                  ),
+                  RadioListTile(
+                    groupValue: taskStatus,
+                    title: const Text('Cancelled'),
+                    value: 'Cancelled',
+                    onChanged: (value) {
+                      taskStatus = value!;
+                      changeState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  if (taskStatusChangeInProgress)
+                    const Center(child: CircularProgressIndicator())
+                  else
+                    ReusableElevatedButton(
+                      text: 'Submit',
+                      onTap: () async {
+                        taskStatusChangeInProgress = true;
+                        changeState(() {});
+                        final response = await NetworkRequester()
+                            .getRequest(Urls.changeStatus(taskId, taskStatus));
+                        taskStatusChangeInProgress = false;
+                        changeState(() {});
+                        if (response['status'] == 'success') {
+                          getNewTaskFromApi();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Status change successful!')));
+                          Navigator.pop(context);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content:
+                                  Text('Status change failed! Try again')));
+                        }
+                      },
+                    )
+                ],
+              ),
+            );
+          });
+        });
   }
 
   Card buildSummaryCard(int counter, String title) {
