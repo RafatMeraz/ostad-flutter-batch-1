b8c0e13bca99ed2121832e7690ac51dbefda6fc3
RafatMeraz <razhossain201@gmail.com>
2023-01-29 22:36:26 +0600

    getx basic introduction

---
+++ lib/second_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second screen'),
+      ),
+    );
+  }
+}
