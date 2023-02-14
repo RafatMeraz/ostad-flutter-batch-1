5f006c6a11685673351e55beab0bfd7110cc556f
RafatMeraz <razhossain201@gmail.com>
2023-02-14 23:34:49 +0600

    google map widget updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,69 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late GoogleMapController googleMapController;
 
+  Set<Marker> markers = <Marker>{};
+
+  @override
+  void initState() {
+    super.initState();
+    markers = {
+      Marker(
+        onTap: () {
+          print('Marker tapped');
+        },
+        flat: false,
+        draggable: true,
+        onDrag: (location) {
+          print(location);
+        },
+        onDragStart: (location) {
+          print(location);
+        },
+        onDragEnd: (location) {
+          print(location);
+        },
+        rotation: 50,
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen),
+        infoWindow: InfoWindow(
+            title: 'BRTA',
+            onTap: () {
+              print('Info window tapped');
+            }
+        ),
+        markerId: MarkerId('abc'),
+        position: LatLng(23.80560326575505, 90.3745761108481),
+      ),
+      Marker(
+        onTap: () {
+          print('Marker tapped');
+        },
+        flat: false,
+        draggable: true,
+        onDrag: (location) {
+          print(location);
+        },
+        onDragStart: (location) {
+          print(location);
+        },
+        onDragEnd: (location) {
+          print(location);
+        },
+        rotation: 50,
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueMagenta),
+        infoWindow: InfoWindow(
+            title: 'Scholastica',
+            onTap: () {
+              print('Info window tapped');
+            }
+        ),
+        markerId: MarkerId('lksfk'),
+        position: LatLng(23.80472443775955, 90.3774966942303),
+      ),
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,8 +111,65 @@ class _HomeState extends State<Home> {
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterDocked,
       body: GoogleMap(
+        mapType: MapType.normal,
+        liteModeEnabled: true,
         myLocationEnabled: true,
+        trafficEnabled: true,
         myLocationButtonEnabled: true,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        markers: markers,
+        polylines: <Polyline> {
+          Polyline(
+              startCap: Cap.roundCap,
+              endCap: Cap.roundCap,
+              visible: true,
+              color: Colors.blueAccent,
+              width: 6,
+              polylineId: PolylineId('sfd'),
+              points: [
+                LatLng(23.80560326575505, 90.3745761108481),
+                LatLng(23.80472443775955, 90.3774966942303),
+                LatLng(23.802806379193132, 90.37997359405793),
+              ],
+          ),
+          Polyline(
+              startCap: Cap.roundCap,
+              endCap: Cap.roundCap,
+              visible: true,
+              color: Colors.red,
+              width: 12,
+              polylineId: PolylineId('sdsad'),
+              onTap: () {
+                print('Tapped polyline');
+              },
+              points: [
+                LatLng(23.802806379193132, 90.37997359405793),
+                LatLng(23.80273514001151, 90.37519285964767),
+              ],
+          ),
+        },
+        // polygons: <Polygon> {
+        //   Polygon(
+        //     polygonId: PolygonId('sfdsdf'),
+        //     points: [
+        //       LatLng(23.80560326575505, 90.3745761108481),
+        //       LatLng(23.80472443775955, 90.3774966942303),
+        //       LatLng(23.802806379193132, 90.37997359405793),
+        //       LatLng(23.80273514001151, 90.37519285964767),
+        //     ]
+        //   )
+        // },
+        circles: <Circle> {
+          Circle(
+            circleId: CircleId('sfdf'),
+            center: LatLng(23.80273514001151, 90.37519285964767),
+            radius: 200,
+            strokeColor: Colors.blue,
+            strokeWidth: 2,
+            fillColor: Colors.transparent
+          )
+        },
         onMapCreated: (GoogleMapController? controller) {
           googleMapController = controller!;
         },
