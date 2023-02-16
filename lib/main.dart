ffe6e6b3710e485457c8eb4bb7ed67afc558fe4a
RafatMeraz <razhossain201@gmail.com>
2023-02-16 23:33:15 +0600

    geolocator code has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -11,7 +12,88 @@ class MyMapApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: LocationScreen(),
+    );
+  }
+}
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+
+  Position? currentLocationLatLng;
+
+  void getCurrentLocation() async {
+    LocationPermission permissionStatus = await Geolocator.checkPermission();
+    print(permissionStatus);
+    if (permissionStatus == LocationPermission.denied) {
+      LocationPermission requestedPermission = await Geolocator.requestPermission();
+      print(requestedPermission);
+    }
+    final locationServiceEnable = await Geolocator.isLocationServiceEnabled();
+    print(locationServiceEnable);
+    currentLocationLatLng = await Geolocator.getCurrentPosition();
+    print(currentLocationLatLng);
+    print(currentLocationLatLng?.isMocked);
+    print(currentLocationLatLng?.heading);
+    print(currentLocationLatLng?.speed);
+    print(currentLocationLatLng?.speedAccuracy);
+    print(currentLocationLatLng?.accuracy);
+    setState(() {
+
+    });
+  }
+
+  listenToUpdatedLocation() {
+    LocationSettings settings = const LocationSettings(
+        accuracy: LocationAccuracy.best,
+        distanceFilter: 5,
+        timeLimit: Duration(seconds: 3));
+    Geolocator.getPositionStream(locationSettings: settings).listen((location) {
+      currentLocationLatLng = location;
+      setState(() {});
+    });
+  }
+
+  listenToLocationServiceStatus() {
+    Geolocator.getServiceStatusStream().listen((status) {
+      print(status);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listenToUpdatedLocation();
+    listenToLocationServiceStatus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Geolocator'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Current Location : $currentLocationLatLng',
+              textAlign: TextAlign.center,
+            ),
+            TextButton(onPressed: () {
+              getCurrentLocation();
+            }, child: const Text('Get location'))
+          ],
+        ),
+      ),
     );
   }
 }
@@ -48,14 +130,12 @@ class _HomeState extends State<Home> {
           print(location);
         },
         rotation: 50,
-        icon: BitmapDescriptor.defaultMarkerWithHue(
-            BitmapDescriptor.hueGreen),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
         infoWindow: InfoWindow(
             title: 'BRTA',
             onTap: () {
               print('Info window tapped');
-            }
-        ),
+            }),
         markerId: MarkerId('abc'),
         position: LatLng(23.80560326575505, 90.3745761108481),
       ),
@@ -75,14 +155,13 @@ class _HomeState extends State<Home> {
           print(location);
         },
         rotation: 50,
-        icon: BitmapDescriptor.defaultMarkerWithHue(
-            BitmapDescriptor.hueMagenta),
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
         infoWindow: InfoWindow(
             title: 'Scholastica',
             onTap: () {
               print('Info window tapped');
-            }
-        ),
+            }),
         markerId: MarkerId('lksfk'),
         position: LatLng(23.80472443775955, 90.3774966942303),
       ),
@@ -119,34 +198,34 @@ class _HomeState extends State<Home> {
         zoomControlsEnabled: true,
         zoomGesturesEnabled: true,
         markers: markers,
-        polylines: <Polyline> {
+        polylines: <Polyline>{
           Polyline(
-              startCap: Cap.roundCap,
-              endCap: Cap.roundCap,
-              visible: true,
-              color: Colors.blueAccent,
-              width: 6,
-              polylineId: PolylineId('sfd'),
-              points: [
-                LatLng(23.80560326575505, 90.3745761108481),
-                LatLng(23.80472443775955, 90.3774966942303),
-                LatLng(23.802806379193132, 90.37997359405793),
-              ],
+            startCap: Cap.roundCap,
+            endCap: Cap.roundCap,
+            visible: true,
+            color: Colors.blueAccent,
+            width: 6,
+            polylineId: PolylineId('sfd'),
+            points: [
+              LatLng(23.80560326575505, 90.3745761108481),
+              LatLng(23.80472443775955, 90.3774966942303),
+              LatLng(23.802806379193132, 90.37997359405793),
+            ],
           ),
           Polyline(
-              startCap: Cap.roundCap,
-              endCap: Cap.roundCap,
-              visible: true,
-              color: Colors.red,
-              width: 12,
-              polylineId: PolylineId('sdsad'),
-              onTap: () {
-                print('Tapped polyline');
-              },
-              points: [
-                LatLng(23.802806379193132, 90.37997359405793),
-                LatLng(23.80273514001151, 90.37519285964767),
-              ],
+            startCap: Cap.roundCap,
+            endCap: Cap.roundCap,
+            visible: true,
+            color: Colors.red,
+            width: 12,
+            polylineId: PolylineId('sdsad'),
+            onTap: () {
+              print('Tapped polyline');
+            },
+            points: [
+              LatLng(23.802806379193132, 90.37997359405793),
+              LatLng(23.80273514001151, 90.37519285964767),
+            ],
           ),
         },
         // polygons: <Polygon> {
@@ -160,15 +239,14 @@ class _HomeState extends State<Home> {
         //     ]
         //   )
         // },
-        circles: <Circle> {
+        circles: <Circle>{
           Circle(
-            circleId: CircleId('sfdf'),
-            center: LatLng(23.80273514001151, 90.37519285964767),
-            radius: 200,
-            strokeColor: Colors.blue,
-            strokeWidth: 2,
-            fillColor: Colors.transparent
-          )
+              circleId: CircleId('sfdf'),
+              center: LatLng(23.80273514001151, 90.37519285964767),
+              radius: 200,
+              strokeColor: Colors.blue,
+              strokeWidth: 2,
+              fillColor: Colors.transparent)
         },
         onMapCreated: (GoogleMapController? controller) {
           googleMapController = controller!;
